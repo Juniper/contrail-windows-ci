363b73ebeb7580df41e37e9c2504d3948c31cd9c
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-03-22 17:21:35 +0100

    Create directory structure in powershell, instead of groovy
    Lessen our dependencies on groovy.
    Don't generate report when XmlsDir doesn't exist.
    Use separate directory for generated output.
    Save some info about files' location in json.

--- CIScripts/GenerateTestReport.ps1
+++ CIScripts/GenerateTestReport.ps1
@@ -1,19 +1,42 @@
-Param([Parameter(Mandatory = $true)] [string] $XmlsDir)
+Param(
+    [Parameter(Mandatory = $true)] [string] $XmlsDir,
+    [Parameter(Mandatory = $true)] [string] $OutputDir
+)
 
+. $PSScriptRoot\Common\Init.ps1
 . $PSScriptRoot\NUnitReportFixup\Repair-NUnitReport.ps1
 
 function Convert-TestReportToHtml {
-    param ([Parameter(Mandatory = $true)] [String[]] $XmlReportDir)
+    param (
+        [Parameter(Mandatory = $true)] [String] $XmlReportDir,
+        [Parameter(Mandatory = $true)] [string] $OutputDir
+    )
 
-    $XmlReports = Get-ChildItem -Filter "$XmlReportDir/*.xml"
-    $XmlReports | ForEach-Object {
-        if (Test-Path $_) {
-            [string] $Content = Get-Content $_
-            $FixedContent = Repair-NUnitReport -InputData $Content
-            $FixedContent | Out-File $_ -Encoding "utf8"
-        }
+    $RawDir = "$OutputDir/raw_NUnit"
+    $PrettyDirName = "pretty_test_report"
+    $PrettyDir = "$OutputDir/$PrettyDirName"
+
+    New-Item -Type Directory -Force $RawDir | Out-Null
+    New-Item -Type Directory -Force $PrettyDir | Out-Null
+
+    foreach ($ReportFile in Get-ChildItem $XmlReportDir -Filter *.xml) {
+        [string] $Content = Get-Content $ReportFile.FullName
+        $FixedContent = Repair-NUnitReport -InputData $Content
+        $FixedContent | Out-File "$RawDir/$($ReportFile.Name)" -Encoding "utf8"
     }
-    ReportUnit.exe $XmlsDir
+    ReportUnit.exe $RawDir
+
+    Move-Item "$RawDir/*.html" $PrettyDir
+
+    $Xmls = Get-ChildItem $RawDir | Foreach-Object { $_.FullName.split('\')[-2 .. -1] -join "/" }
+    @{
+        xml_reports = $Xmls
+        html_report = "$PrettyDirName/Index.html"
+    } | ConvertTo-Json -Depth 10 | Out-File "$OutputDir/reports-locations.json"
 }
 
-Convert-TestReportToHtml -XmlReportDir $XmlsDir
+if (Test-Path $XmlsDir) {
+    Convert-TestReportToHtml -XmlReportDir $XmlsDir -OutputDir $OutputDir
+} else {
+    Write-Warning "No report generated, directory $XmlsDir doesn't exist"
+}
