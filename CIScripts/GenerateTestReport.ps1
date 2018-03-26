e70c52ccb43ec17ebf67690f7680b985605c2b9c
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-03-26 20:20:09 +0200

    Refactor and reorganize GenerateTestReport
    * Splitted functions as requested in review
    * Moved report-related functions to Report dir
    * Added tests to GenerateTestReport

--- CIScripts/GenerateTestReport.ps1
+++ CIScripts/GenerateTestReport.ps1
@@ -4,39 +4,10 @@ Param(
 )
 
 . $PSScriptRoot\Common\Init.ps1
-. $PSScriptRoot\NUnitReportFixup\Repair-NUnitReport.ps1
+. $PSScriptRoot\Report\GenerateTestReport.ps1
 
-function Convert-TestReportToHtml {
-    param (
-        [Parameter(Mandatory = $true)] [String] $XmlReportDir,
-        [Parameter(Mandatory = $true)] [string] $OutputDir
-    )
-
-    $RawDir = "$OutputDir/raw_NUnit"
-    $PrettyDirName = "pretty_test_report"
-    $PrettyDir = "$OutputDir/$PrettyDirName"
-
-    New-Item -Type Directory -Force $RawDir | Out-Null
-    New-Item -Type Directory -Force $PrettyDir | Out-Null
-
-    foreach ($ReportFile in Get-ChildItem $XmlReportDir -Filter *.xml) {
-        [string] $Content = Get-Content $ReportFile.FullName
-        $FixedContent = Repair-NUnitReport -InputData $Content
-        $FixedContent | Out-File "$RawDir/$($ReportFile.Name)" -Encoding "utf8"
-    }
-    ReportUnit.exe $RawDir
-
-    Move-Item "$RawDir/*.html" $PrettyDir
-
-    $Xmls = Get-ChildItem $RawDir | Foreach-Object { $_.FullName.split('\')[-2 .. -1] -join "/" }
-    @{
-        xml_reports = $Xmls
-        html_report = "$PrettyDirName/Index.html"
-    } | ConvertTo-Json -Depth 10 | Out-File "$OutputDir/reports-locations.json" -Encoding "utf8"
-}
-
-if (Test-Path $XmlsDir) {
-    Convert-TestReportToHtml -XmlReportDir $XmlsDir -OutputDir $OutputDir
+if ((Test-Path $XmlsDir) -and (Get-ChildItem $XmlsDir)) {
+    Convert-TestReportsToHtml -XmlReportsDir $XmlsDir -OutputDir $OutputDir
 } else {
-    Write-Warning "No report generated, directory $XmlsDir doesn't exist"
+    Write-Warning "No report generated, directory $XmlsDir doesn't exist or is empty"
 }
