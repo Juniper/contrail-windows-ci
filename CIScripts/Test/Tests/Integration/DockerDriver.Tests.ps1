b703b5bf8cf6377d24c92e5a5e74d969c9d3eabb
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-04-12 14:03:28 +0200

    Fix usage of Write-Log in DockerDriver.Tests

--- CIScripts/Test/Tests/Integration/DockerDriver.Tests.ps1
+++ CIScripts/Test/Tests/Integration/DockerDriver.Tests.ps1
@@ -5,6 +5,7 @@ Param (
 
 . $PSScriptRoot\..\..\..\Common\Aliases.ps1
 . $PSScriptRoot\..\..\..\Common\VMUtils.ps1
+. $PSScriptRoot\..\..\..\Common\Invoke-NativeCommand.ps1
 
 . $PSScriptRoot\..\..\PesterLogger\PesterLogger.ps1
 Initialize-PesterLogger -OutDir $LogDir
@@ -14,7 +15,7 @@ $Session = $Sessions[0]
 
 $TestsPath = "C:\Artifacts\"
 
-function Start-DockerDriverUnitTest {
+function Invoke-DockerDriverUnitTest {
     Param (
         [Parameter(Mandatory=$true)] [PSSessionT] $Session,
         [Parameter(Mandatory=$true)] [string] $Component
@@ -24,27 +25,18 @@ function Start-DockerDriverUnitTest {
     $Command = @($TestFilePath, "--ginkgo.noisyPendings", "--ginkgo.failFast", "--ginkgo.progress", "--ginkgo.v", "--ginkgo.trace")
     $Command = $Command -join " "
 
-    $Res = Invoke-Command -Session $Session -ScriptBlock {
+    $Res = Invoke-NativeCommand -CaptureOutput -AllowNonZero -Session $Session {
         Push-Location $Using:TestsPath
-
-        # Invoke-Command used as a workaround for temporary ErrorActionPreference modification
-        $Res = Invoke-Command -ScriptBlock {
-            if (Test-Path $Using:TestFilePath) {
-                $ErrorActionPreference = "SilentlyContinue"
-                $Output = Invoke-Expression -Command $Using:Command
-                Write-Log $Output
-                return $LASTEXITCODE
-            } else {
-                return 1
-            }
+        try {
+            Invoke-Expression -Command $Using:Command
+        } finally {
+            Pop-Location
         }
-
-        Pop-Location
-
-        return $Res
     }
 
-    return $Res
+    Write-Log $Res.Output
+
+    return $Res.ExitCode
 }
 
 function Save-DockerDriverUnitTestReport {
@@ -53,22 +45,24 @@ function Save-DockerDriverUnitTestReport {
         [Parameter(Mandatory=$true)] [string] $Component
     )
 
+    # TODO Are these junit files actually created?
+    # TODO Where are these files copied to?
     Copy-Item -FromSession $Session -Path ($TestsPath + $Component + "_junit.xml") -ErrorAction SilentlyContinue
 }
 
 # TODO: these modules should also be tested: controller, hns, hnsManager, driver
-$modules = @("agent")
+$Modules = @("agent")
 
 Describe "Docker Driver" {
-    $modules | ForEach-Object {
-        Context "Tests for module $_" {
+    foreach ($Module in $Modules) {
+        Context "Tests for module $Module" {
             It "Tests are invoked" {
-                $TestResult = Start-DockerDriverUnitTest -Session $Session -Component $_
+                $TestResult = Invoke-DockerDriverUnitTest -Session $Session -Component $Module
                 $TestResult | Should Be 0
             }
 
             AfterEach {
-                Save-DockerDriverUnitTestReport -Session $Session -Component $_
+                Save-DockerDriverUnitTestReport -Session $Session -Component $Module
             }
         }
     }
