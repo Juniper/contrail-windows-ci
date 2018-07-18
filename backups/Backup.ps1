9133f027efb86e6ad2a87a1f6a854b612a910dbf
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-07-18 12:45:34 +0200

    backups: Remove-LastBackups function; main backup script

---
+++ backups/Backup.ps1
@@ -0,0 +1,22 @@
+. $PSScriptRoot\Backup-Infrastructure.ps1
+
+$Repository = "\\10.84.10.100\vol\winci_backup\Backups"
+$PreserveCount = 5
+
+$VirtualMachines = @(
+    [VMSpec]@{Name="ci-vc"},
+    [VMSpec]@{Name="ci-vc-um"},
+    [VMSpec]@{Name="mgmt-dhcp"},
+    [VMSpec]@{Name="winci-choco"},
+    [VMSpec]@{Name="winci-drive"},
+    [VMSpec]@{Name="winci-jenkins"},
+    [VMSpec]@{Name="winci-mgmt"},
+    [VMSpec]@{Name="winci-purgatory-1"},
+    [VMSpec]@{Name="winci-purgatory-2"},
+    [VMSpec]@{Name="winci-registry"},
+    [VMSpec]@{Name="winci-zuulv2-production"},
+    [VMSpec]@{Name="winci-vyos-mgmt"; SupportQuiesce = $false}
+)
+
+Backup-Infrastructure -VirtualMachines $VirtualMachines -Repository $Repository
+Remove-LastBackups -Repository $backupRepository -PreserveCount $PreserveCount
