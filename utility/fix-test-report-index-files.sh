e0ce5f9b528dbcabd7b3daad68e15c4c60df37a3
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-07-31 16:25:47 +0200

    fix-index-files: properly populate array in bash

--- utility/fix-test-report-index-files.sh
+++ utility/fix-test-report-index-files.sh
@@ -7,7 +7,9 @@ main()
     local test_reports
 
     test_reports="$1"
-    index_files=$(find "${test_reports}" -name 'Index.html')
+
+    # Output is converted to array by enclosing it in ( and )
+    index_files=( $(find "${test_reports}" -name 'Index.html') )
     for f in "${index_files[@]}"; do
         local d
 
