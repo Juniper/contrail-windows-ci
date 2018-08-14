e101bb9fa301257a242be187cd1b613e823f0420
Michał Cłapiński <michal.clapinski@codilime.com>
2018-08-14 19:14:39 +0200

    added defaults for zuul

--- CIScripts/Checkout.ps1
+++ CIScripts/Checkout.ps1
@@ -4,10 +4,28 @@
 
 $Job = [Job]::new("Checkout")
 
-Get-ZuulRepos -GerritUrl $Env:GERRIT_URL `
-              -ZuulProject $Env:ZUUL_PROJECT `
-              -ZuulRef $Env:ZUUL_REF `
-              -ZuulUrl $Env:ZUUL_URL `
-              -ZuulBranch $Env:ZUUL_BRANCH
+$defaults = @{
+    GERRIT_URL = "https://review.opencontrail.org"
+    ZUUL_PROJECT = "controller"
+    ZUUL_REF = "None"
+    ZUUL_URL = "https://review.opencontrail.org"
+    ZUUL_BRANCH = "master"
+}
+
+ForEach ($elem in $defaults.GetEnumerator()) {
+    $envVarName = "Env:$($elem.Key)"
+    if (Test-Path $envVarName) {
+        $value = (Get-ChildItem $envVarName).Value
+    } else {
+        $value = $elem.Value
+    }
+    Set-Variable -Name $elem.Key -Value $value
+}
+
+Get-ZuulRepos -GerritUrl $GERRIT_URL `
+              -ZuulProject $ZUUL_PROJECT `
+              -ZuulRef $ZUUL_REF `
+              -ZuulUrl $ZUUL_URL `
+              -ZuulBranch $ZUUL_BRANCH
 
 $Job.Done()
