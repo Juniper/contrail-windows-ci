258d50a56dfc0c2cb577ce88a55bbd14db7d99dd
Michał Cłapiński <michal.clapinski@codilime.com>
2018-08-16 19:27:23 +0200

    typo

--- CIScripts/Checkout/Zuul.ps1
+++ CIScripts/Checkout/Zuul.ps1
@@ -13,7 +13,7 @@ function Get-ZuulRepos {
         $GerritUrl
     )
 
-    if ($ZuulParams.Count -ne 0) {
+    if ($ZuulAdditionalParams.Count -ne 0) {
         $ZuulClonerOptions = @(
             "--zuul-url=$($ZuulAdditionalParams.Url)",
             "--zuul-project=$($ZuulAdditionalParams.Project)",
