135461496a4d6703445f2e1f892d28fb9df8a0b5
Michał Cłapiński <michal.clapinski@codilime.com>
2018-08-16 19:25:19 +0200

    lexical errors

--- CIScripts/Checkout.ps1
+++ CIScripts/Checkout.ps1
@@ -6,15 +6,15 @@ $Job = [Job]::new("Checkout")
 
 if (Test-Path "Env:ZUUL_URL") {
     $ZuulAdditionalParams = @{
-        Url = $Env:ZUUL_URL,
-        Project = $Env:ZUUL_PROJECT,
+        Url = $Env:ZUUL_URL;
+        Project = $Env:ZUUL_PROJECT;
         Ref = $Env:ZUUL_REF
     }
     Get-ZuulRepos -GerritUrl $Env:GERRIT_URL `
                   -ZuulBranch $Env:ZUUL_BRANCH `
                   -ZuulAdditionalParams $ZuulAdditionalParams
 } else {
-    Get-ZuulRepos -GerritUrl $Env:GERRIT_URL
+    Get-ZuulRepos -GerritUrl $Env:GERRIT_URL `
                   -ZuulBranch $Env:ZUUL_BRANCH
 }
 
