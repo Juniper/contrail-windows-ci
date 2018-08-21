17048569fa308ea92c61603b38a62e67d42e5766
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-08-21 16:43:01 +0200

    Provision floating ip, without association; Test still fails

--- Test/Utils/ContrailUtils.ps1
+++ Test/Utils/ContrailUtils.ps1
@@ -106,3 +106,43 @@ function Remove-ContrailFloatingIpPool {
     $RequestUrl = $ContrailUrl + "/floating-ip-pool/" + $PoolUuid
     Invoke-RestMethod -Uri $RequestUrl -Headers @{"X-Auth-Token" = $AuthToken} -Method Delete | Out-Null
 }
+
+function Add-ContrailFloatingIp {
+    Param ([Parameter(Mandatory = $true)] [string] $ContrailUrl,
+           [Parameter(Mandatory = $true)] [string] $AuthToken,
+           [Parameter(Mandatory = $true)] [string] $PoolUuid,
+           [Parameter(Mandatory = $true)] [string] $IPAddress)
+
+    $PoolUrl = $ContrailUrl + "/floating-ip-pool/" + $PoolUuid
+    $Pool = Invoke-RestMethod -Uri $PoolUrl -Headers @{"X-Auth-Token" = $AuthToken}
+
+    $PoolFqName = $Pool."floating-ip-pool".fq_name
+    $FipFqName = $PoolFqName + "fip"
+
+    $Request = @{
+        "floating-ip" = @{
+            "floating_ip_address" = $IPAddress
+            "fq_name" = $FipFqName
+            "parent_type" = "floating-ip-pool"
+            "uuid" = $null
+            # "virtual_machine_interface_refs" = ADD
+        }
+    }
+    $RequestUrl = $ContrailUrl + "/floating-ips"
+    $Response = Invoke-RestMethod `
+        -Uri $RequestUrl `
+        -Headers @{"X-Auth-Token" = $AuthToken} `
+        -Method Post `
+        -ContentType "application/json" `
+        -Body (ConvertTo-Json -Depth $CONVERT_TO_JSON_MAX_DEPTH $Request)
+    return $Response.'floating-ip'.'uuid'
+}
+
+function Remove-ContrailFloatingIp {
+    Param ([Parameter(Mandatory = $true)] [string] $ContrailUrl,
+           [Parameter(Mandatory = $true)] [string] $AuthToken,
+           [Parameter(Mandatory = $true)] [string] $IpUuid)
+
+    $RequestUrl = $ContrailUrl + "/floating-ip/" + $IpUuid
+    Invoke-RestMethod -Uri $RequestUrl -Headers @{"X-Auth-Token" = $AuthToken} -Method Delete | Out-Null
+}
