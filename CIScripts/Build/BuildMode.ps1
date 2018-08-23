d7a5a58f9307fbcee6da854a80a3264be519e7f3
Arkadiusz Kudan <arkadiusz.kudan@codilime.com>
2018-08-23 18:07:37 +0200

    Add debug print

--- CIScripts/Build/BuildMode.ps1
+++ CIScripts/Build/BuildMode.ps1
@@ -1,6 +1,8 @@
 function Resolve-BuildMode {
     #Helper function to verify in what build configuration/mode
     #(release/production or debug) Windows compute node components will be built.
+    Write-Host $Env:BUILD_IN_RELEASE_MODE
+
     $IsReleaseMode = [bool]::Parse($Env:BUILD_IN_RELEASE_MODE)
     $BuildMode = $(if ($IsReleaseMode) { "production" } else { "debug" })
 
