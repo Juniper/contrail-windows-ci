7db4d0b56749733f1418deafe960ef1855ecfd52
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2018-09-26 13:09:31 +0200

    Comments

--- Test/TestConfigurationUtils.ps1
+++ Test/TestConfigurationUtils.ps1
@@ -166,10 +166,14 @@ function Stop-DockerDriver {
     Invoke-Command -Session $Session -ScriptBlock {
         Stop-Service docker | Out-Null
 
+        # Removing NAT objects when 'winnat' service is stopped may fail.
+        # In this case, we have to try removing all objects but ignore failures for some of them.
         Get-NetNat | ForEach-Object {
             Remove-NetNat $_.Name -Confirm:$false -ErrorAction SilentlyContinue
         }
 
+        # Removing ContainerNetworks may fail for NAT network when 'winnat'
+        # service is disabled, so we have to filter out all NAT networks.
         Get-ContainerNetwork | Where-Object Name -NE nat | Remove-ContainerNetwork -ErrorAction SilentlyContinue -Force
         Get-ContainerNetwork | Where-Object Name -NE nat | Remove-ContainerNetwork -Force
 
