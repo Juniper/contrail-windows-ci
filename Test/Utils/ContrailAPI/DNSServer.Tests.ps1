e01b3b612b27e46caee157f1b3b1f6c44bc840e4
jacekchalupka <jacek.chalupka@codilime.com>
2018-10-16 00:13:39 +0200

    Split DNS related functions into two files (plus one improvement)
    Setting DNS mode options are now passed as implementation
    of DNSOptions interface.

--- Test/Utils/ContrailAPI/DNSServer.Tests.ps1
+++ Test/Utils/ContrailAPI/DNSServer.Tests.ps1
@@ -3,7 +3,8 @@ Param (
     [Parameter(ValueFromRemainingArguments=$true)] $UnusedParams
 )
 
-. $PSScriptRoot\ConfigureDNS.ps1
+. $PSScriptRoot\DNSServer.ps1
+. $PSScriptRoot\SettingDNSMode.ps1
 . $PSScriptRoot\..\ContrailUtils.ps1
 . $PSScriptRoot\..\..\..\CIScripts\Testenv\Testenv.ps1
 
@@ -80,7 +81,7 @@ Describe 'Configure DNS API' -Tags CI, Systest {
 
             Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
                 -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                -DNSMode 'virtual-dns-server' -VirtualServerName "CreatedByPS1ScriptForce"
+                -DNSMode 'virtual-dns-server' -Options ([VirtualDNSOptions]::New("CreatedByPS1ScriptForce"))
 
             {
                 Remove-ContrailDNSServer -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
@@ -91,77 +92,6 @@ Describe 'Configure DNS API' -Tags CI, Systest {
                 Remove-ContrailDNSServer -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
                     -DNSServerUuid $ServerUUID -Force
             } | Should -Not -Throw
-        }
-    }
-
-    Context 'Setting DNS modes' {
-        It 'throws when wrong dns mode specified' {
-            {
-                Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                    -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                    -DNSMode 'wrongdnsmode'
-            } | Should -Throw
-        }
-
-        Context 'mode - none' {
-            It 'can set DNS mode to none' {
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'none'
-                } | Should -Not -Throw
-            }
-        }
-
-        Context 'mode - default' {
-            It 'can set DNS mode to default' {
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'default-dns-server'
-                } | Should -Not -Throw
-            }
-        }
-
-        Context 'mode - tenant' {
-            It 'can set DNS mode to tenant without DNS servers' {
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'tenant-dns-server'
-                } | Should -Not -Throw
-            }
-
-            It 'can set DNS mode to tenant with DNS servers' {
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'tenant-dns-server' -TenantServersIPAddresses @("1.1.1.1", "2.2.2.2")
-                } | Should -Not -Throw
-            }
-        }
-
-        Context 'mode - virtual' {
-            It 'can set DNS "mode" to virtual' {
-                $ServerUUID = Add-ContrailDNSServer -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                    -DNSServerName "CreatedByPS1ScriptForIpam"
-
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'virtual-dns-server' -VirtualServerName "CreatedByPS1ScriptForIpam"
-                } | Should -Not -Throw
-
-                Remove-ContrailDNSServer -ContrailUrl $ContrailUrl -AuthToken $AuthToken -DNSServerUuid $ServerUUID -Force
-            }
-
-            It 'throws when no virtual DNS server specified in virtual DNS mode' {
-                {
-                    Set-IpamDNSMode -ContrailUrl $ContrailUrl -AuthToken $AuthToken `
-                        -IpamFQName @("default-domain", "default-project", "default-network-ipam") `
-                        -DNSMode 'virtual-dns-server'
-                } | Should -Throw
-            }
         }
     }
 }
