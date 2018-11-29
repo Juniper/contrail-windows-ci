b9a9afc607f0b2f86bd5a6bacc24c944b28c0749
Magda Zaremba <magdalena.zaremba@codilime.com>
2018-11-29 12:37:54 +0100

    Stop services before unistalling them

--- Test/Tests/Component/NodeManager.Tests.ps1
+++ Test/Tests/Component/NodeManager.Tests.ps1
@@ -137,6 +137,7 @@ Describe "Node manager" -Tag "Smoke" {
         if (Get-Variable "MultiNode" -ErrorAction SilentlyContinue) {
 
             if ($PrepareEnv) {
+                Clear-ComputeNode -Session $Session -SystemConfig $MultiNode.Configs
                 Uninstall-Components -Session $Session
                 Clear-Logs -LogSources $FileLogSources
             }
