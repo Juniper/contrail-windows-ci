2ad1de6cc7b675d66f1377b5535d0ccaf036db67
Jacek Chalupka <jacek.chalupka@codilime.com>
2018-12-06 18:06:09 +0100

    New Contrail API

---
+++ Test/Utils/ContrailAPI_New/ContrailRestApi.ps1
@@ -0,0 +1,113 @@
+class ContrailRestApi {
+    [Int] $CONVERT_TO_JSON_MAX_DEPTH = 100;
+
+    [String] $AuthToken;
+    [String] $ContrailUrl;
+    [String] $DefaultTenantName;
+
+    ContrailRestApi([TestenvConfigs] $TestenvConfig) {
+
+        $this.ContrailUrl = $TestenvConfig.Controller.RestApiUrl()
+        $this.DefaultTenantName = $TestenvConfig.Controller.DefaultProject
+
+        if ($TestenvConfig.Controller.AuthMethod -eq 'keystone') {
+            if (!$TestenvConfig.OpenStack) {
+                throw 'AuthMethod is keystone, but no OpenStack config provided.'
+            }
+
+            $this.AuthToken = $this.GetAccessTokenFromKeystone($TestenvConfig.OpenStack)
+        }
+        elseif ($TestenvConfig.Controller.AuthMethod -eq 'noauth') {
+            $this.AuthToken = $null
+        }
+        else {
+            throw "Unknown authentification method: $($TestenvConfig.Controller.AuthMethod). Supported: keystone, noauth."
+        }
+    }
+
+    hidden [String] GetAccessTokenFromKeystone([OpenStackConfig] $OpenStackConfig) {
+        $Request = @{
+            auth = @{
+                tenantName          = $OpenStackConfig.Project
+                passwordCredentials = @{
+                    username = $OpenStackConfig.Username
+                    password = $OpenStackConfig.Password
+                }
+            }
+        }
+        $AuthUrl = $OpenStackConfig.AuthUrl() + '/tokens'
+        $Response = Invoke-RestMethod -Uri $AuthUrl -Method Post -ContentType 'application/json' `
+            -Body (ConvertTo-Json -Depth $this.CONVERT_TO_JSON_MAX_DEPTH $Request)
+        return $Response.access.token.id
+    }
+
+    [PSObject] GetResourceUrl([String] $Resource, [String] $Uuid) {
+        $RequestUrl = $this.ContrailUrl + '/' + $Resource
+
+        if (-not $Uuid) {
+            $RequestUrl += 's'
+        }
+        else {
+            $RequestUrl += ('/' + $Uuid)
+        }
+
+        return $RequestUrl
+    }
+
+    hidden [PSObject] SendRequest([String] $Method, [String] $Resource,
+        [String] $Uuid, $Request) {
+
+        $RequestUrl = $this.GetResourceUrl($Resource, $Uuid)
+
+        # We need to escape '<>' in 'direction' field because reasons
+        # http://www.azurefieldnotes.com/2017/05/02/replacefix-unicode-characters-created-by-convertto-json-in-powershell-for-arm-templates/
+        $Body = (ConvertTo-Json -Depth $this.CONVERT_TO_JSON_MAX_DEPTH $Request |
+                ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) })
+        $Headers = $null
+        $HeadersString = $null
+        if ($this.AuthToken) {
+            $Headers = @{'X-Auth-Token' = $this.AuthToken}
+        }
+
+        if ($Headers) {
+            $HeadersString = $Headers.GetEnumerator()  | ForEach-Object { "$($_.Name): $($_.Value)" }
+        }
+        Write-Log "[Contrail][$Method]=>[$RequestUrl]"
+        Write-Log -NoTimestamp -NoTag "Headers:`n$HeadersString;`nBody:`n$Body"
+
+        $Response = Invoke-RestMethod -Uri $RequestUrl -Headers $Headers `
+            -Method $Method -ContentType 'application/json' `
+            -Body $Body
+        Write-Log '[Contrail]<= '
+        Write-Log -NoTimestamp -NoTag "$Response"
+        return $Response
+    }
+
+    [PSObject] Get([String] $Resource, [String] $Uuid, $Request) {
+        return $this.SendRequest('Get', $Resource, $Uuid, $Request)
+    }
+
+    [PSObject] Post([String] $Resource, [String] $Uuid, $Request) {
+        return $this.SendRequest('Post', $Resource, $Uuid, $Request)
+    }
+
+    [PSObject] Put([String] $Resource, [String] $Uuid, $Request) {
+        return $this.SendRequest('Put', $Resource, $Uuid, $Request)
+    }
+
+    [Void] Delete([String] $Resource, [String] $Uuid, $Request) {
+        $this.SendRequest('Delete', $Resource, $Uuid, $Request)
+    }
+
+    [String] FQNameToUuid ([string] $Resource, [string[]] $FQName) {
+        $Request = @{
+            type    = $Resource
+            fq_name = $FQName
+        }
+
+        $RequestUrl = $this.ContrailUrl + '/fqname-to-id'
+        $Response = Invoke-RestMethod -Uri $RequestUrl -Headers @{'X-Auth-Token' = $this.AuthToken} `
+            -Method 'Post' -ContentType 'application/json' -Body (ConvertTo-Json -Depth $this.CONVERT_TO_JSON_MAX_DEPTH $Request)
+        return $Response.'uuid'
+    }
+}
---
+++ Test/Utils/ContrailAPI_New/Repos/ContrailRepo.ps1
@@ -0,0 +1,72 @@
+class ContrailRepo {
+    [ContrailRestApi] $API
+
+    ContrailRepo([ContrailRestApi] $API) {
+        $this.API = $API
+    }
+
+    [PSobject] Add([BaseResourceModel] $Object) {
+        $Request = $Object.GetRequest()
+        $Request.$($Object.ResourceName) += @{
+            parent_type = $Object.ParentType
+            fq_name     = $Object.GetFQName()
+        }
+
+        return $this.API.Post($Object.ResourceName, $null, $Request)
+    }
+
+    [PSobject] AddOrReplace([BaseResourceModel] $Object) {
+        try {
+            return $this.Add($Object)
+        }
+        catch {
+            if ($_.Exception.Response.StatusCode -ne [System.Net.HttpStatusCode]::Conflict) {
+                throw
+            }
+        }
+        $this.RemoveWithDependencies($Object)
+        return $this.Add($Object)
+    }
+
+    [PSobject] Set([BaseResourceModel] $Object) {
+        $Uuid = $this.API.FQNameToUuid($Object.ResourceName, $Object.GetFQName())
+        $Request = $Object.GetRequest()
+
+        return $this.API.Put($Object.ResourceName, $Uuid, $Request)
+    }
+
+    [Void] RemoveWithDependencies([BaseResourceModel] $Object) {
+        $this.RemoveObject($Object, $true)
+    }
+
+    [Void] Remove([BaseResourceModel] $Object) {
+        $this.RemoveObject($Object, $false)
+    }
+
+    Hidden [Void] RemoveObject([BaseResourceModel] $Object, [bool] $WithDependencies) {
+        $Uuid = $this.API.FQNameToUuid($Object.ResourceName, $Object.GetFQName())
+
+        if ($WithDependencies) {
+            $this.RemoveDependencies($Object)
+        }
+
+        $this.API.Delete($Object.ResourceName, $Uuid, $null) | Out-Null
+    }
+
+    hidden [void] RemoveDependencies([BaseResourceModel] $Object) {
+        if (-not $Object.Dependencies) {
+            return
+        }
+        $Uuid = $this.API.FQNameToUuid($Object.ResourceName, $Object.GetFQName())
+        $Response = $this.API.Get($Object.ResourceName, $Uuid, $null)
+        $Props = $Response.$($Object.ResourceName).PSobject.Properties.Name
+
+        ForEach ($Dependency in $Object.Dependencies) {
+            if ($Props -contains $Dependency.ReferencesField) {
+                ForEach ($Child in $Response.$($Object.ResourceName).$($Dependency.ReferencesField)) {
+                    $this.API.Delete($Dependency.ResourceName, $Child.'uuid', $null)
+                }
+            }
+        }
+    }
+}
