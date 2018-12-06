d671cce383a0d5bce59cbd83ddaac8c4542499c2
Jacek Chalupka <jacek.chalupka@codilime.com>
2018-12-06 21:39:51 +0100

    New Contrail API - Virtual Router

---
+++ Test/Utils/ContrailAPI_New/Models/VirtualRouterModel.ps1
@@ -0,0 +1,25 @@
+class VirtualRouter : BaseResourceModel {
+    [String] $Name
+    [String] $Ip
+    [String] $ParentName = 'default-global-system-config'
+
+    VirtualRouter([String] $Name, [String] $Ip) {
+        $this.Name = $Name
+        $this.Ip = $Ip
+    }
+
+    [String[]] GetFqName() {
+        return @($this.ParentName, $this.Name)
+    }
+
+    [String] $ResourceName = 'virtual-router'
+    [String] $ParentType = 'global-system-config'
+
+    [Hashtable] GetRequest() {
+        return @{
+            'virtual-router' = @{
+                virtual_router_ip_address = $this.Ip
+            }
+        }
+    }
+}
