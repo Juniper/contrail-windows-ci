e8c47902025dca1c2eaf3996a7d01c193b23f7e2
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2018-12-11 15:37:59 +0100

    New Contrail API - Security Group (#417)

---
+++ Test/Utils/ContrailAPI_New/SecurityGroup.ps1
@@ -0,0 +1,8 @@
+# Dotsource needed files above this line
+. $PSScriptRoot\ContrailRestApi.ps1
+. $PSScriptRoot\Models\BaseResourceModel.ps1
+. $PSScriptRoot\Repos\ContrailRepo.ps1
+. $PSScriptRoot\Models\AddressModel.ps1
+. $PSScriptRoot\Models\PortRangeModel.ps1
+. $PSScriptRoot\Models\PolicyRuleModel.ps1
+. $PSScriptRoot\Models\SecurityGroupModel.ps1
