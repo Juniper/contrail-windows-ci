95430008ad7ebf720f09c9245f59b8180a4fa6d9
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2018-12-12 13:27:24 +0100

    New Contrail API - Address, PolicyRule, PortRange, Security Group (#416)

---
+++ Test/Utils/ContrailAPI_New/Models/PortRangeModel.ps1
@@ -0,0 +1,18 @@
+class PortRange {
+    [Int] $StartPort
+    [Int] $EndPort
+
+    [Hashtable] GetRequest() {
+        return @{
+            start_port = $this.StartPort
+            end_port   = $this.EndPort
+        }
+    }
+
+    static [PortRange] new_Full() {
+        $range = [PortRange]::new()
+        $range.StartPort = 0
+        $range.EndPort = 65535
+        return $range
+    }
+}
