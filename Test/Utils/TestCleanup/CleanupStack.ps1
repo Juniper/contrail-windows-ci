a805abc2badfcde4a6f1653e2573bef27f2e7534
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2018-12-19 19:29:46 +0100

    Implementation of cleanup stack (#434)

---
+++ Test/Utils/TestCleanup/CleanupStack.ps1
@@ -0,0 +1,40 @@
+class CleanupStack {
+    [System.Collections.Stack] $Stack = [System.Collections.Stack]::new()
+    [ContrailRepo] $ContrailRepo = $null
+
+    [Void] Push([ScriptBlock] $ScriptBlock, [PSobject[]] $Arguments) {
+        $this.Stack.Push([FunctionObject]::new($ScriptBlock, $Arguments))
+    }
+
+    [Void] Push([BaseResourceModel] $ModelObject) {
+        $this.Stack.Push($ModelObject)
+    }
+
+    [PSobject] Pop() {
+        return $this.Stack.Pop()
+    }
+
+    [PSobject] Peek() {
+        return $this.Stack.Peek()
+    }
+
+    [Void] RunCleanup([ContrailRepo] $ContrailRepo) {
+        $this.ContrailRepo = $ContrailRepo
+        foreach ($RemoveObject in $this.Stack) {
+            try {
+                $this.Remove($RemoveObject)
+            }
+            catch {
+                Write-Log (Out-String -InputObject $_)
+            }
+        }
+    }
+
+    [Void] Remove([FunctionObject] $FunctionObject) {
+        Invoke-Command -ScriptBlock $FunctionObject.ScriptBlock -ArgumentList $FunctionObject.Arguments
+    }
+
+    [Void] Remove([BaseResourceModel] $ResourceModel) {
+        $this.ContrailRepo.RemoveWithDependencies($ResourceModel) | Out-Null
+    }
+}
