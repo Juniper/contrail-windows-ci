25e790d5f8480e92c29c7748657cf4fd9e807232
Jacek Chalupka <jacek.chalupka@codilime.com>
2018-12-28 19:20:16 +0100

    Fix

--- CIScripts/Testenv/Testbed.ps1
+++ CIScripts/Testenv/Testbed.ps1
@@ -1,5 +1,6 @@
 . $PSScriptRoot\..\Common\Aliases.ps1
 . $PSScriptRoot\..\Common\Credentials.ps1
+. $PSScriptRoot\Configs.ps1
 
 # Dotsource needed files above this line
 . $PSScriptRoot\Testbed.Impl.ps1
