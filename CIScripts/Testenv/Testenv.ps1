94bf8a8327cafe7fbf4f6dd0fdf577736f899972
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2019-01-03 13:57:03 +0100

    Update Testenv.ps1

--- CIScripts/Testenv/Testenv.ps1
+++ CIScripts/Testenv/Testenv.ps1
@@ -5,5 +5,6 @@
 . $PSScriptRoot\..\..\Test\Utils\TestCleanup\TestCleanup.ps1
 . $PSScriptRoot\..\..\Test\Utils\ContrailNetworkManager.ps1
 . $PSScriptRoot\..\..\Test\Utils\MultiNode\MultiNode.ps1
+. $PSScriptRoot\..\..\Test\PesterLogger\RemoteLogCollector.ps1
 
 . $PSScriptRoot\Testenv.Impl.ps1
