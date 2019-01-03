ac4f98b1f68145b1abe858338158763160b7b33c
Jacek Chalupka <jacek.chalupka@codilime.com>
2019-01-03 11:52:33 +0100

    New initialization that will be used for refactored tests

--- Test/Utils/ContrailAPI_New/ContrailAPI.ps1
+++ Test/Utils/ContrailAPI_New/ContrailAPI.ps1
@@ -7,20 +7,20 @@
 . $PSScriptRoot\Models\Address.ps1
 . $PSScriptRoot\Models\PortRange.ps1
 . $PSScriptRoot\Models\PolicyRule.ps1
-# . $PSScriptRoot\Models\Subnet.ps1
+. $PSScriptRoot\Models\Subnet.ps1
 
 # Resource models
-# . $PSScriptRoot\Models\DNSRecord.ps1
-# . $PSScriptRoot\Models\DNSServer.ps1
-# . $PSScriptRoot\Models\FloatingIp.ps1
-# . $PSScriptRoot\Models\FloatingIpPool.ps1
-# . $PSScriptRoot\Models\GlobalVrouterConfig.ps1
-# . $PSScriptRoot\Models\Ipam.ps1
-# . $PSScriptRoot\Models\NetworkPolicy.ps1
+. $PSScriptRoot\Models\DNSRecord.ps1
+. $PSScriptRoot\Models\DNSServer.ps1
+. $PSScriptRoot\Models\FloatingIp.ps1
+. $PSScriptRoot\Models\FloatingIpPool.ps1
+. $PSScriptRoot\Models\GlobalVrouterConfig.ps1
+. $PSScriptRoot\Models\Ipam.ps1
+. $PSScriptRoot\Models\NetworkPolicy.ps1
 . $PSScriptRoot\Models\Project.ps1
 . $PSScriptRoot\Models\SecurityGroup.ps1
-# . $PSScriptRoot\Models\VirtualNetwork.ps1
+. $PSScriptRoot\Models\VirtualNetwork.ps1
 . $PSScriptRoot\Models\VirtualRouter.ps1
 
 # Repositories
-# . $PSScriptRoot\Repos\VirtualNetworkRepo.ps1
+. $PSScriptRoot\Repos\VirtualNetworkRepo.ps1
