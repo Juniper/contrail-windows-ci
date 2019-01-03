c7e8a5b701af99a5b533411d19b571cede0a5bc1
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2019-01-03 13:03:36 +0100

    Update Testenv.ps1

--- CIScripts/Testenv/Testenv.ps1
+++ CIScripts/Testenv/Testenv.ps1
@@ -3,6 +3,7 @@
 
 . $PSScriptRoot\..\..\Test\Utils\ComputeNode\TestsRequirements.ps1
 . $PSScriptRoot\..\..\Test\Utils\TestCleanup\TestCleanup.ps1
+. $PSScriptRoot\..\..\Test\Utils\ContrailNetworkManager.ps1
 . $PSScriptRoot\..\..\Test\Utils\MultiNode\MultiNode.ps1
 
 . $PSScriptRoot\Testenv.Impl.ps1
