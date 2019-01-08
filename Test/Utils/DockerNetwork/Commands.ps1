cfc8e4967ea35d772e8417483f9930a0ba3cd8ea
Jacek Chalupka <jacek.chalupka@codilime.com>
2019-01-08 14:21:26 +0100

    Passing correct type in Initialize-DockerNetworks

--- Test/Utils/DockerNetwork/Commands.ps1
+++ Test/Utils/DockerNetwork/Commands.ps1
@@ -4,12 +4,7 @@ function Initialize-DockerNetworks {
     Param (
         [Parameter(Mandatory=$true)] [PSSessionT] $Session,
         [Parameter(Mandatory=$true)] [String] $TenantName,
-        # Not refactored tests passes here different type
-        # than already refactored. Fortunately in scope of
-        # usage in this function, they share interfaces.
-        # TODO: Change PSobject to VirtualNetwork after
-        #       tests refactor is finished.
-        [Parameter(Mandatory=$true)] [PSobject[]] $Networks
+        [Parameter(Mandatory=$true)] [VirtualNetwork[]] $Networks
     )
     foreach ($Network in $Networks) {
         $ID = New-DockerNetwork -Session $Session `
