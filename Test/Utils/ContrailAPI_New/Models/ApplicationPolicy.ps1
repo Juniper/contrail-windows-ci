67526c0240f1b93abb94593fe100343a67b89539
Arkadiusz Kudan <arkadiusz.kudan@codilime.com>
2019-01-10 19:01:51 +0100

    Introduce 'application-policy-set' Contrail API resource

---
+++ Test/Utils/ContrailAPI_New/Models/ApplicationPolicy.ps1
@@ -0,0 +1,53 @@
+class ApplicationPolicy : BaseResourceModel {
+    [FqName] $ParentFqName
+    [String] $ResourceName = 'application-policy-set'
+    [String] $ParentType = 'project'
+    [FqName[]] $TagFqNames
+    [FqName[]] $FirewallPolicyFqNames
+
+    ApplicationPolicy([FqName[]] $FirewallPolicyFqNames, [FqName[]] $TagFqNames, [String] $Name, [String] $ProjectName) {
+        $this.Name = $Name
+        $this.ParentFqName = [FqName]::new(@('default-domain', $ProjectName))
+        $this.Tags = $TagFqNames
+        $this.FirewallPolicyFqNames = $FirewallPolicyFqNames
+    }
+
+    hidden [Hashtable[]] GetFPReferences() {
+        $References = @()
+        foreach ($FPFqName in $this.FirewallPolicyFqNames) {
+            $Ref = @{
+                "to" = $FPFqName.ToStringArray()
+                "attr" = @{
+                    "sequence" = "0"
+                }
+            }
+            $References += $Ref
+        }
+
+        return $References
+    }
+
+    hidden [Hashtable[]] GetTagReferences() {
+        $References = @()
+        foreach ($TagFqName in $this.TagFqNames) {
+            $Ref = @{
+                "to" = $TagFqName.ToStringArray()
+            }
+            $References += $Ref
+        }
+
+        return $References
+    }
+
+
+    [Hashtable] GetRequest() {
+        $Request = @{
+            $this.ResourceName = @{}
+        }
+
+        $Request.$($this.ResourceName).Add('firewall_policy_refs', $this.GetFPReferences())
+        $Request.$($this.ResourceName).Add('tag_refs', $this.GetTagReferences())
+
+        return $Request
+    }
+}
