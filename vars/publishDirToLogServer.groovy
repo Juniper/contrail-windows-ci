f63324eee723b2483eab7d65b583714566a527a4
Przemyslaw Marcinkiewicz <przemyslaw.marcinkiewicz@codilime.com>
2019-01-10 17:13:44 +0100

    Test publish log from tentacle job

--- vars/publishDirToLogServer.groovy
+++ vars/publishDirToLogServer.groovy
@@ -1,5 +1,5 @@
 def call(String srcDir, String authority, String destDir) {
     def remoteDir = authority + ":" + destDir
-    shellCommand "ssh", [authority, "mkdir", "-p", destDir]
+    //shellCommand "ssh", [authority, "mkdir", "-p", destDir]
     shellCommand "rsync", ["--prune-empty-dirs", "-r", srcDir + "/", remoteDir]
 }
