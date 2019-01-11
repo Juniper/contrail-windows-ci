cbd2b1ee102603487e9cd0cbda677428be9b5e89
Przemyslaw Marcinkiewicz <przemyslaw.marcinkiewicz@codilime.com>
2019-01-11 10:44:57 +0100

    Fix

--- vars/publishDirToLogServer.groovy
+++ vars/publishDirToLogServer.groovy
@@ -1,5 +1,4 @@
 def call(String srcDir, String authority, String destDir) {
     def remoteDir = authority + ":" + destDir
-    //shellCommand "ssh", [authority, "mkdir", "-p", destDir]
     shellCommand "rsync", ["--prune-empty-dirs", "-r", srcDir + "/", remoteDir]
 }
