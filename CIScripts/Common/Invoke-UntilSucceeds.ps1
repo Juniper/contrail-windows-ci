2a92c7e53ba89109a08279f5ce78b058ea0f2d95
Jacek Chałupka <43140842+jacekchalupka@users.noreply.github.com>
2019-01-14 18:08:44 +0100

    Invoke-UntilSucceeds timeouts even if ScriptBlock hangs (#452)

--- CIScripts/Common/Invoke-UntilSucceeds.ps1
+++ CIScripts/Common/Invoke-UntilSucceeds.ps1
@@ -7,12 +7,10 @@ class HardError : System.Exception {
     HardError([string] $msg, [System.Exception] $inner) : base($msg, $inner) {}
 }
 
-$DebugTag = "[DEBUG Invoke-UntilSucceeds]"
-
 function Invoke-UntilSucceeds {
     <#
     .SYNOPSIS
-    Repeatedly calls a script block until its return value evaluates to true. Subsequent calls
+    Repeatedly calls a script block as a job until its return value evaluates to true. Subsequent calls
     happen after Interval seconds. Will catch any exceptions that occur in the meantime.
     If the exception being thrown is a HardError, no further retry attempps will be made.
     User has to specify a timeout after which the function fails by setting the Duration (or NumRetires) parameter.
@@ -47,6 +45,9 @@ function Invoke-UntilSucceeds {
         [Parameter(Mandatory=$false)] [String] $Name = "Invoke-UntilSucceds",
         [Switch] $AssumeTrue
     )
+
+    $DebugTag = "[DEBUG Invoke-UntilSucceeds]"
+
     Write-Log "$DebugTag Function begins with job: $name"
     Write-Log "$DebugTag Duration: $Duration; NumRetries $NumRetries"
     if ((-not $Duration) -and (-not $NumRetries)) {
@@ -83,9 +84,50 @@ function Invoke-UntilSucceeds {
 
         try {
             Write-Log "$DebugTag Running task. LastCheck: $LastCheck"
-            $ReturnVal = Invoke-Command $ScriptBlock
+
+            $Runspace = [RunspaceFactory]::CreateRunspace()
+            $Runspace.Open()
+
+            $Exception = $null
+            $Runspace.SessionStateProxy.SetVariable('ScriptBlock', $ScriptBlock)
+            $Runspace.SessionStateProxy.SetVariable('Exception', [ref]$Exception)
+
+            $PowerShellThread = [PowerShell]::Create().AddScript( {
+                    try {
+                        . $ScriptBlock
+                    }
+                    catch {
+                        $Exception.Value = $_.Exception
+                    }
+                })
+            $PowerShellThread.Runspace = $Runspace
+            
+            $TimeElapsed = ((Get-Date) - $StartTime).TotalSeconds
+            Write-Log "$DebugTag Starting thread. TimeElapsed: $TimeElapsed"
+            $ThreadHandle = $PowerShellThread.BeginInvoke()
+
+            if ($Duration) {
+                [System.Threading.WaitHandle]::WaitAny($ThreadHandle.AsyncWaitHandle, $Duration * 1000) | Out-Null
+                $TimeElapsed = ((Get-Date) - $StartTime).TotalSeconds
+                Write-Log "$DebugTag Finished waiting thread. TimeElapsed: $TimeElapsed"
+                if (-not $ThreadHandle.IsCompleted) {
+                    $PowerShellThread.Stop()
+                    $TimeElapsed = ((Get-Date) - $StartTime).TotalSeconds
+                    throw "Job didn't finish in $Duration seconds. After $TimeElapsed we stopped trying."
+                }
+            }
+            else {
+                [System.Threading.WaitHandle]::WaitAny($ThreadHandle.AsyncWaitHandle) | Out-Null
+            }
+
+            if ($null -ne $Exception) {
+                throw $Exception
+            }
+            $ReturnVal = $PowerShellThread.EndInvoke($ThreadHandle)
+
             Write-Log "$DebugTag Task returned with ReturnVal: $ReturnVal"
             if ($AssumeTrue -or $ReturnVal) {
+                Write-Log "$DebugTag Returning value"
                 return $ReturnVal
             } else {
                 throw [CITimeoutException]::new(
@@ -93,11 +135,13 @@ function Invoke-UntilSucceeds {
                 )
             }
         } catch [HardError] {
+            Write-Log "$DebugTag Caught HardError. $($_.Exception.InnerException)"
             throw [CITimeoutException]::new(
                 "${Name}: Stopped retrying because HardError was thrown",
                 $_.Exception.InnerException
             )
         } catch {
+            Write-Log "$DebugTag Caught 'soft' exception. $($_.Exception)"
             if ($LastCheck) {
                 throw [CITimeoutException]::new("$Name failed.", $_.Exception)
             } else {
