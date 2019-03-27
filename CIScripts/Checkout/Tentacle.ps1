f8d489a43791918cfb0fc5b116961f9ea58bbab9
Jacek Iżykowski <jacek.izykowski@codilime.com>
2019-03-27 16:04:32 +0100

    Fix: move contrail-windows-test to Test

--- CIScripts/Checkout/Tentacle.ps1
+++ CIScripts/Checkout/Tentacle.ps1
@@ -14,5 +14,5 @@ function Get-TentacleRepos {
         Expand-Archive -Path repos.zip -DestinationPath .
     })
 
-    Move-Item -Force -Path src/Juniper/contrail-windows-test -Destination Test
+    Move-Item -Force -Path contrail-windows-test -Destination Test
 }
