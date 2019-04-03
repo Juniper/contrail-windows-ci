8dfb20538f1122e33ad2ff2cf51584f00606fe75
Michał Cłapiński <michal.clapinski@codilime.com>
2019-04-03 16:11:29 +0200

    New redistributable for OpenSSL (#571)

--- vars/getTestbedTemplate.groovy
+++ vars/getTestbedTemplate.groovy
@@ -1,6 +1,6 @@
 def call() {
     def TESTBED_TEMPLATES = [
-        '2016': 'Template-testbed-201903190303',
+        '2016': 'Template-testbed-201904020832',
         '2019': 'Template-testbed2019-201903190401'
     ]
 
